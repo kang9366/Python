7d709b12e486ba3667042e10adc38497c6a984f7
SEUNGGU KANG <63611804+kang9366@users.noreply.github.com>
2020-09-09 21:38:27 +0900

    Add files via upload

---
+++ crawling/Naver movie rank/movie_rank.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=cnt&date='
+year = input('input year for 4 digits : ')
+month = input('input month for 2 digits : ')
+date = input('input date for 2 digits : ')
+
+d_url = url + year + month + date
+
+data = requests.get(d_url, headers = headers).text
+soup = BeautifulSoup(data, 'html.parser')
+td = soup.find_all('div', 'tit3')
+
+title = []
+for i in td:
+    temp = []
+    temp.append(i.find('a').text)
+    title.append(temp)
+
+print('\n' + str(year) + '년 ' + str(month) + '월 ' + str(date) + '일 영화 순위' + '\n')
+
+for i in range(len(title)):
+    if i < 9:
+        print(' ' + str(i + 1) + '위 : ', end = "")
+        print(", ".join(title[i]))
+    else : 
+        print(str(i + 1) + '위 : ', end = "")
+        print(", ".join(title[i]))
+
+os.system('Pause')
+
+print('hello world')
